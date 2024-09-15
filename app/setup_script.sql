9fc5ad06b1ab6f38261b44a9327fcc81e3cea24f
Jerry Tang <jerry.r.tang@gmail.com>
2024-09-14 19:12:20 -0700

    completed snowflake native app tutorial

---
+++ app/setup_script.sql
@@ -0,0 +1,67 @@
+-- This is the setup script that runs while installing a Snowflake Native App in a consumer account.
+-- To write this script, you can familiarize yourself with some of the following concepts:
+-- Application Roles
+-- Versioned Schemas
+-- UDFs/Procs
+-- Extension Code
+-- Refer to https://docs.snowflake.com/en/developer-guide/native-apps/creating-setup-script for a detailed understanding of this file. 
+
+CREATE OR ALTER VERSIONED SCHEMA core;
+
+-- The rest of this script is left blank for purposes of your learning and exploration. 
+
+-- create the application role aop_public
+CREATE APPLICATION ROLE IF NOT EXISTS app_public;
+CREATE SCHEMA IF NOT EXISTS core;
+GRANT USAGE ON SCHEMA core TO APPLICATION ROLE app_public;
+
+-- create a procedure
+CREATE OR REPLACE PROCEDURE CORE.HELLO()
+  RETURNS STRING
+  LANGUAGE SQL
+  EXECUTE AS OWNER
+  AS
+  BEGIN
+    RETURN 'Hello Snowflake!';
+  END;
+
+GRANT USAGE ON PROCEDURE core.hello() TO APPLICATION ROLE app_public;
+
+CREATE OR ALTER VERSIONED SCHEMA code_schema;
+GRANT USAGE ON SCHEMA code_schema TO APPLICATION ROLE app_public;
+
+-- create a view for app_public 
+CREATE VIEW IF NOT EXISTS code_schema.accounts_view
+  AS SELECT ID, NAME, VALUE
+  FROM shared_data.accounts;
+GRANT SELECT ON VIEW code_schema.accounts_view TO APPLICATION ROLE app_public;
+
+-- add an inline python UDF
+CREATE OR REPLACE FUNCTION code_schema.addone(i int)
+RETURNS INT
+LANGUAGE PYTHON
+RUNTIME_VERSION = '3.11'
+HANDLER = 'addone_py'
+AS
+$$
+def addone_py(i):
+  return i+1
+$$;
+GRANT USAGE ON FUNCTION code_schema.addone(int) TO APPLICATION ROLE app_public;
+
+-- add an external python module 
+CREATE or REPLACE FUNCTION code_schema.multiply(num1 float, num2 float)
+  RETURNS float
+  LANGUAGE PYTHON
+  RUNTIME_VERSION=3.8
+  IMPORTS = ('/python/hello_python.py')
+  HANDLER='hello_python.multiply';
+
+GRANT USAGE ON FUNCTION code_schema.multiply(FLOAT, FLOAT) TO APPLICATION ROLE app_public;
+
+-- add an streamlit app
+CREATE STREAMLIT IF NOT EXISTS code_schema.hello_joy_streamlit
+  FROM '/streamlit'
+  MAIN_FILE = '/hello_joy.py'
+;
+GRANT USAGE ON STREAMLIT code_schema.hello_joy_streamlit TO APPLICATION ROLE app_public;
